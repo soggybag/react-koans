692edae554591f8c1c1fb426622cfc7a2bb8a87d
Mitchell <soggybag@gmail.com>
2021-05-07 09:15:13 -0700

    Init!

---
+++ src/01-Hello.test.js
@@ -0,0 +1,26 @@
+
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Hello from "./01-Hello";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders with or without a name", () => {
+  act(() => {
+    render(<Hello />, container);
+  });
+  expect(container.textContent).toBe("Hello World");
+});
